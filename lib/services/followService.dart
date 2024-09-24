4a12dc32a7a498efbd36db6f4553ac638a4c3f4c
Adam <adamsaifi.cs@gmail.com>
2024-09-24 17:03:54 +0300

    added two methods getFollowerRequests and cancelFollowerRequest

--- lib/services/followService.dart
+++ lib/services/followService.dart
@@ -2,12 +2,12 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class FollowService {
-  static const String baseUrl = 'https://da8f-185-97-92-77.ngrok-free.app/api/Users';
+  static const String baseUrl = '***REMOVED***/api/Users';
 
   // Follow a user
-  Future<void> followUser(int followerUserId, int userId) async {
+  Future<void> followUser(int followerUserId, int followed_userId) async {
     final body = jsonEncode({
-      'user_id': userId,
+      'followed_user_id': followed_userId,
       'follower_user_id': followerUserId,
     });
 
@@ -27,9 +27,9 @@ class FollowService {
   }
 
   // Unfollow a user
-  Future<void> unfollowUser(int followerUserId, int userId) async {
+  Future<void> unfollowUser(int followerUserId, int followed_userId) async {
     final body = jsonEncode({
-      'user_id': userId,
+      'followed_user_id': followed_userId,
       'follower_user_id': followerUserId,
     });
 
@@ -45,6 +45,31 @@ class FollowService {
       }
     } catch (e) {
       throw Exception('Error occurred while unfollowing user: $e');
+    }
+  }
+  // Cancel a follower request
+  Future<void> cancelFollowerRequest(int followerUserId, int followedUserId) async {
+    final body = jsonEncode({
+      'followed_user_id': followedUserId,
+      'follower_user_id': followerUserId,
+    });
+
+    try {
+      // Make sure the URL and HTTP method are correct
+      final response = await http.post(
+        Uri.parse('$baseUrl/cancel-follower-request'),
+        headers: {'Content-Type': 'application/json'},
+        body: body,
+      );
+
+      print('Response status code: ${response.statusCode}');
+      print('Response body: ${response.body}');
+
+      if (response.statusCode != 200) {
+        throw Exception('Failed to cancel follower request: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Error occurred while canceling follower request: $e');
     }
   }
 }
