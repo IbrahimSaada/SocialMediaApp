5b313504493d8dc5260c752aa2fa530124bb8100
Ahmad Ghosen <ahmadghosen20@gmail.com>
2024-09-24 19:27:21 +0300

    Edit on design and add more transaction

--- lib/home/add_friends_page.dart
+++ lib/home/add_friends_page.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:shimmer/shimmer.dart'; // For shimmer effect
+import 'package:shimmer/shimmer.dart';
 import '***REMOVED***/models/SearchUserModel.dart';
 import '***REMOVED***/services/search_service.dart';
-import '***REMOVED***/services/loginservice.dart'; // Import LoginService
-import '***REMOVED***/services/followService.dart'; // Import FollowService
+import '***REMOVED***/services/loginservice.dart';
+import '***REMOVED***/services/followService.dart';
 
 class AddFriendsPage extends StatefulWidget {
   @override
   _AddFriendsPageState createState() => _AddFriendsPageState();
 }
 
-class _AddFriendsPageState extends State<AddFriendsPage> with TickerProviderStateMixin {
+class _AddFriendsPageState extends State<AddFriendsPage>
+    with TickerProviderStateMixin {
   List<SearchUserModel> users = [];
   Map<String, String> requestStatus = {};
   bool isLoading = true;
-  bool isLoadingMore = false; // For infinite scroll
+  bool isLoadingMore = false;
   final SearchService _searchService = SearchService();
   final LoginService _loginService = LoginService();
   final FollowService _followService = FollowService();
@@ -24,7 +25,7 @@ class _AddFriendsPageState extends State<AddFriendsPage> with TickerProviderStat
   void initState() {
     super.initState();
     _loadFollowerRequests();
-    _scrollController.addListener(_scrollListener); // Attach scroll listener
+    _scrollController.addListener(_scrollListener);
   }
 
   @override
@@ -33,19 +34,20 @@ class _AddFriendsPageState extends State<AddFriendsPage> with TickerProviderStat
     super.dispose();
   }
 
-  // Infinite scroll listener
   void _scrollListener() {
-    if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent && !isLoadingMore) {
+    if (_scrollController.position.pixels ==
+            _scrollController.position.maxScrollExtent &&
+        !isLoadingMore) {
       _loadMoreFollowers();
     }
   }
 
-  // Initial load of follower requests
   Future<void> _loadFollowerRequests() async {
     try {
       int? currentUserId = await _loginService.getUserId();
       if (currentUserId != null) {
-        List<SearchUserModel> fetchedUsers = await _searchService.getFollowerRequests(currentUserId);
+        List<SearchUserModel> fetchedUsers =
+            await _searchService.getFollowerRequests(currentUserId);
         setState(() {
           users = fetchedUsers;
           isLoading = false;
@@ -64,15 +66,27 @@ class _AddFriendsPageState extends State<AddFriendsPage> with TickerProviderStat
     }
   }
 
-  // Load more follower requests for infinite scroll
   Future<void> _loadMoreFollowers() async {
     setState(() {
       isLoadingMore = true;
     });
 
-    // You can implement pagination here later if necessary.
-    
+    await Future.delayed(Duration(seconds: 2));
+
     setState(() {
+      users.addAll(List.generate(
+          5,
+          (index) => SearchUserModel(
+                userId: users.length + index,
+                fullName: 'User ${users.length + index}',
+                username: 'user${users.length + index}',
+                profilePic: 'assets/profile.png',
+                bio: 'This is the bio for user ${users.length + index}',
+                phoneNumber: '123-456-789${index}',
+                isFollowing: false,
+                amFollowing: false,
+              )));
+
       isLoadingMore = false;
     });
   }
@@ -103,11 +117,13 @@ class _AddFriendsPageState extends State<AddFriendsPage> with TickerProviderStat
               children: [
                 // "Add Friends" header with classy font and icons
                 Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Icon(Icons.restaurant_menu, color: Colors.white, size: 30),
+                      Icon(Icons.restaurant_menu,
+                          color: Colors.white, size: 30),
                       SizedBox(width: 10),
                       Text(
                         'Add Friends',
@@ -119,7 +135,8 @@ class _AddFriendsPageState extends State<AddFriendsPage> with TickerProviderStat
                         ),
                       ),
                       SizedBox(width: 10),
-                      Icon(Icons.restaurant_menu, color: Colors.white, size: 30),
+                      Icon(Icons.restaurant_menu,
+                          color: Colors.white, size: 30),
                     ],
                   ),
                 ),
@@ -138,20 +155,39 @@ class _AddFriendsPageState extends State<AddFriendsPage> with TickerProviderStat
                         ? _shimmerLoading() // Show shimmer while loading
                         : NotificationListener<ScrollNotification>(
                             onNotification: (ScrollNotification scrollInfo) {
-                              if (!isLoadingMore && scrollInfo.metrics.pixels == scrollInfo.metrics.maxScrollExtent) {
+                              if (!isLoadingMore &&
+                                  scrollInfo.metrics.pixels ==
+                                      scrollInfo.metrics.maxScrollExtent) {
                                 _loadMoreFollowers();
                               }
                               return true;
                             },
                             child: ListView.builder(
                               controller: _scrollController,
-                              itemCount: users.length + (isLoadingMore ? 1 : 0), // Add loading indicator at the bottom if loading more
+                              itemCount: users.length +
+                                  (isLoadingMore
+                                      ? 1
+                                      : 0), // Add loading indicator at the bottom if loading more
                               itemBuilder: (context, index) {
                                 if (index == users.length && isLoadingMore) {
-                                  return Center(child: CircularProgressIndicator());
+                                  return Center(
+                                      child: CircularProgressIndicator());
                                 }
                                 final user = users[index];
-                                return _friendRequestCard(user.fullName, user.username, user.userId);
+                                return _friendRequestCard(
+                                  user.fullName,
+                                  user.username,
+                                  user.userId,
+                                  user.bio,
+                                  user.phoneNumber,
+                                  (username) {
+                                    setState(() {
+                                      // Remove the user from the list when declined
+                                      users.removeWhere(
+                                          (u) => u.username == username);
+                                    });
+                                  },
+                                );
                               },
                             ),
                           ),
@@ -165,7 +201,6 @@ class _AddFriendsPageState extends State<AddFriendsPage> with TickerProviderStat
     );
   }
 
-  // Shimmer effect while loading
   Widget _shimmerLoading() {
     return ListView.builder(
       itemCount: 5,
@@ -174,8 +209,9 @@ class _AddFriendsPageState extends State<AddFriendsPage> with TickerProviderStat
           baseColor: Colors.grey[300]!,
           highlightColor: Colors.grey[100]!,
           child: Container(
-            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-            height: 80,
+            margin: EdgeInsets.symmetric(
+                vertical: 5, horizontal: 20), // Reduced vertical margin
+            height: 70, // Reduced height
             decoration: BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.circular(20),
@@ -186,73 +222,87 @@ class _AddFriendsPageState extends State<AddFriendsPage> with TickerProviderStat
     );
   }
 
-  // Friend request card with buttons below the full name and orange shaded border
-  Widget _friendRequestCard(String fullName, String username, int followedUserId) {
-    final user = users.firstWhere((u) => u.username == username); // Find the user model for additional properties
-
+  Widget _friendRequestCard(
+      String fullName,
+      String username,
+      int followedUserId,
+      String bio,
+      String phoneNumber,
+      Function(String) onDecline) {
     return Container(
-      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-      padding: EdgeInsets.all(15),
+      margin: EdgeInsets.symmetric(
+          vertical: 5, horizontal: 20), // Reduced vertical margin
+      padding: EdgeInsets.all(10), // Reduced padding
       decoration: BoxDecoration(
         color: Colors.white,
-        border: Border.all(color: Colors.orangeAccent, width: 2), // Orange border with shading
+        border: Border.all(color: Colors.orange, width: 2),
         borderRadius: BorderRadius.circular(20),
         boxShadow: [
           BoxShadow(
-            color: Colors.orangeAccent.withOpacity(0.3),
-            blurRadius: 15,
-            spreadRadius: 5,
+            color: Colors.orange.withOpacity(0.3),
+            blurRadius: 10, // Reduced blur radius
+            spreadRadius: 3, // Reduced spread radius
             offset: Offset(0, 5),
           ),
         ],
       ),
-      child: Column(
+      child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(
-            children: [
-              CircleAvatar(
-                backgroundImage: user.profilePic.isNotEmpty 
-                  ? NetworkImage(user.profilePic) // Use NetworkImage if profilePic is a URL
-                  : AssetImage('assets/profile.png') as ImageProvider, // Fallback to AssetImage if no profilePic
-                radius: 28,
-              ),
-              SizedBox(width: 15),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+          CircleAvatar(
+            backgroundImage: AssetImage('assets/profile.png'),
+            radius: 25, // Reduced radius
+          ),
+          SizedBox(width: 10), // Reduced spacing
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text(
-                      fullName,
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            fullName,
+                            style: TextStyle(
+                              fontSize: 16, // Reduced font size
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                          Text('@$username',
+                              style: TextStyle(
+                                  fontSize: 12, color: Colors.grey[600])),
+                          SizedBox(height: 2), // Reduced spacing
+                          Text(bio,
+                              style: TextStyle(
+                                  fontSize: 10, color: Colors.grey[800])),
+                          Text(phoneNumber,
+                              style: TextStyle(
+                                  fontSize: 10, color: Colors.grey[800])),
+                        ],
                       ),
                     ),
-                    Text(
-                      '@$username',
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: Colors.grey[600],
-                      ),
-                    ),
+                    // Action buttons on the same line
+                    _actionButtons(username, followedUserId, onDecline),
                   ],
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-          SizedBox(height: 15),
-          _actionButtons(username, followedUserId),
         ],
       ),
     );
   }
 
-  // Action buttons placed below the full name
-  Widget _actionButtons(String username, int followedUserId) {
+  Widget _actionButtons(
+      String username, int followedUserId, Function(String) onDecline) {
+    bool isFollowing = requestStatus[username] == 'Following';
+
     return Row(
-      mainAxisAlignment: MainAxisAlignment.end,
       children: [
         ElevatedButton(
           onPressed: () async {
@@ -269,34 +319,40 @@ class _AddFriendsPageState extends State<AddFriendsPage> with TickerProviderStat
             }
           },
           style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.orangeAccent,
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
-            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            backgroundColor: Colors.green,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
           ),
-          child: Text('Follow', style: TextStyle(color: Colors.white)),
+          child: Text(isFollowing ? 'Following' : 'Follow',
+              style: TextStyle(color: Colors.white)),
         ),
-        SizedBox(width: 10),
-        ElevatedButton(
-          onPressed: () async {
-            try {
-              int? currentUserId = await _loginService.getUserId();
-              if (currentUserId != null) {
-                await _followService.cancelFollowerRequest(followedUserId, currentUserId);
-                setState(() {
-                  requestStatus[username] = 'Declined';
-                });
+        SizedBox(width: 5), // Reduced spacing
+        if (!isFollowing) // Show "Decline" button only if not following
+          ElevatedButton(
+            onPressed: () async {
+              try {
+                int? currentUserId = await _loginService.getUserId();
+                if (currentUserId != null) {
+                  await _followService.cancelFollowerRequest(
+                      followedUserId, currentUserId);
+                  setState(() {
+                    requestStatus[username] = 'Declined';
+                  });
+                  onDecline(username); // Call the decline function
+                }
+              } catch (e) {
+                print('Error while canceling the follower request: $e');
               }
-            } catch (e) {
-              print('Error while canceling the follower request: $e');
-            }
-          },
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.redAccent,
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
-            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.redAccent,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18)),
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            ),
+            child: Text('Decline', style: TextStyle(color: Colors.white)),
           ),
-          child: Text('Decline', style: TextStyle(color: Colors.white)),
-        ),
       ],
     );
   }
