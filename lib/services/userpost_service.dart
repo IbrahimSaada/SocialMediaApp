63638236d8e5125e937b6109bed51350500d5289
Adam <adamsaifi.cs@gmail.com>
2024-09-30 23:06:02 +0300

    Real posts in profile and thumbnails for caption

---
+++ lib/services/userpost_service.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/post_model.dart';
+
+class UserpostService {
+  static const String baseUrl = '***REMOVED***/api';
+
+  // Fetch User Posts
+  Future<List<Post>> fetchUserPosts(int userId, int pageNumber, int pageSize) async {
+    final url = Uri.parse('$baseUrl/UserProfile?userId=$userId&pageNumber=$pageNumber&pageSize=$pageSize');
+
+    final response = await http.get(url, headers: {
+      'Content-Type': 'application/json',
+    });
+
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonData = json.decode(response.body);
+      return jsonData.map((json) => Post.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load user posts');
+    }
+  }
+
+  // Fetch Bookmarked Posts
+  Future<List<Post>> fetchBookmarkedPosts(int userId, int pageNumber, int pageSize) async {
+    final url = Uri.parse('$baseUrl/UserProfile/bookmarked?userId=$userId&pageNumber=$pageNumber&pageSize=$pageSize');
+
+    final response = await http.get(url, headers: {
+      'Content-Type': 'application/json',
+    });
+
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonData = json.decode(response.body);
+      return jsonData.map((json) => Post.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load bookmarked posts');
+    }
+  }
+}
