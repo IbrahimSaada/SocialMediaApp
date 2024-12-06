e550bf47434e3fbc576ff3e1177be68f5111a903
Adam <adamsaifi.cs@gmail.com>
2024-12-06 15:06:22 +0200

    Not related to notification, Login/registeration all files are respoonsive and Single Scrolling

--- lib/login/forgotpasswrod.dart
+++ lib/login/forgotpasswrod.dart
@@ -41,42 +41,35 @@ class ForgotpasswrodhomePage extends StatelessWidget {
     }
 
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            colors: [
-              primaryColor.withOpacity(0.9),
-              primaryColor.withOpacity(0.8),
-              primaryColor.withOpacity(0.4),
-            ],
+      body: SingleChildScrollView(
+        child: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              colors: [
+                primaryColor.withOpacity(0.9),
+                primaryColor.withOpacity(0.8),
+                primaryColor.withOpacity(0.4),
+              ],
+            ),
           ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const SizedBox(
-              height: 80,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  FadeInUp(
-                    duration: const Duration(milliseconds: 1000),
-                    child: const Text(
-                      "Find your account",
-                      style: TextStyle(color: Colors.white, fontSize: 40),
-                    ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(height: 80),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: FadeInUp(
+                  duration: const Duration(milliseconds: 1000),
+                  child: const Text(
+                    "Find your account",
+                    style: TextStyle(color: Colors.white, fontSize: 40),
                   ),
-                ],
+                ),
               ),
-            ),
-            const SizedBox(height: 20),
-            Expanded(
-              child: Container(
+              const SizedBox(height: 20),
+              Container(
                 decoration: const BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
@@ -86,116 +79,106 @@ class ForgotpasswrodhomePage extends StatelessWidget {
                 ),
                 child: Padding(
                   padding: const EdgeInsets.all(30),
-                  child: SingleChildScrollView(
-                    child: Form(
-                      key: formKey,
-                      child: Column(
-                        children: <Widget>[
-                          const SizedBox(
-                            height: 60,
-                          ),
-                          FadeInUp(
-                            duration: const Duration(milliseconds: 1400),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(10),
-                                boxShadow: const [
-                                  BoxShadow(
-                                    color: Color.fromRGBO(225, 95, 27, .3),
-                                    blurRadius: 20,
-                                    offset: Offset(0, 10),
-                                  ),
-                                ],
-                              ),
-                              child: Column(
-                                children: <Widget>[
-                                  Container(
-                                    padding: const EdgeInsets.all(10),
-                                    decoration: BoxDecoration(
-                                      border: Border(
-                                        bottom: BorderSide(
-                                          color: Colors.grey.shade200,
-                                        ),
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      children: <Widget>[
+                        const SizedBox(height: 60),
+                        FadeInUp(
+                          duration: const Duration(milliseconds: 1400),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: Color.fromRGBO(225, 95, 27, .3),
+                                  blurRadius: 20,
+                                  offset: Offset(0, 10),
+                                ),
+                              ],
+                            ),
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  padding: const EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                    border: Border(
+                                      bottom: BorderSide(
+                                        color: Colors.grey.shade200,
                                       ),
                                     ),
-                                    child: TextFormField(
-                                      controller: emailOrPhoneController,
-                                      decoration: const InputDecoration(
-                                        hintText:
-                                            "Enter your email or phone number",
-                                        hintStyle:
-                                            TextStyle(color: Colors.grey),
-                                        border: InputBorder.none,
-                                      ),
-                                      validator: (value) {
-                                        if (value == null || value.isEmpty) {
-                                          return 'Please enter your email or phone number';
-                                        } else if (!RegExp(
-                                                    r'^[^@]+@[^@]+\.[^@]+$')
-                                                .hasMatch(value) &&
-                                            !RegExp(r'^\+?[0-9]{10,15}$')
-                                                .hasMatch(value)) {
-                                          return 'Please enter a valid email or phone number';
-                                        }
-                                        return null;
-                                      },
-                                    ),
                                   ),
-                                ],
-                              ),
+                                  child: TextFormField(
+                                    controller: emailOrPhoneController,
+                                    decoration: const InputDecoration(
+                                      hintText:
+                                          "Enter your email or phone number",
+                                      hintStyle: TextStyle(color: Colors.grey),
+                                      border: InputBorder.none,
+                                    ),
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        return 'Please enter your email or phone number';
+                                      } else if (!RegExp(r'^[^@]+@[^@]+\.[^@]+$')
+                                              .hasMatch(value) &&
+                                          !RegExp(r'^\+?[0-9]{10,15}$')
+                                              .hasMatch(value)) {
+                                        return 'Please enter a valid email or phone number';
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-                          const SizedBox(
-                            height: 40,
-                          ),
-                          FadeInUp(
-                            duration: const Duration(milliseconds: 1600),
-                            child: MaterialButton(
-                              onPressed: sendResetCode,
-                              height: 50,
-                              color: primaryColor,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(50),
-                              ),
-                              child: const Center(
-                                child: Text(
-                                  "Continue",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                  ),
+                        ),
+                        const SizedBox(height: 40),
+                        FadeInUp(
+                          duration: const Duration(milliseconds: 1600),
+                          child: MaterialButton(
+                            onPressed: sendResetCode,
+                            height: 50,
+                            color: primaryColor,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                            child: const Center(
+                              child: Text(
+                                "Continue",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
                                 ),
                               ),
                             ),
                           ),
-                          const SizedBox(
-                            height: 40,
-                          ),
-                          FadeInUp(
-                            duration: const Duration(milliseconds: 1500),
-                            child: GestureDetector(
-                              onTap: () {
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(
-                                    builder: (context) => const LoginPage(),
-                                  ),
-                                );
-                              },
-                              child: const Text(
-                                "Back to login page",
-                                style: TextStyle(color: Colors.grey),
-                              ),
+                        ),
+                        const SizedBox(height: 40),
+                        FadeInUp(
+                          duration: const Duration(milliseconds: 1500),
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => const LoginPage(),
+                                ),
+                              );
+                            },
+                            child: const Text(
+                              "Back to login page",
+                              style: TextStyle(color: Colors.grey),
                             ),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
