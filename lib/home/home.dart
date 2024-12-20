2677e2cde1691423399887c597b016683aae9fe6
Adam <adamsaifi.cs@gmail.com>
2024-12-20 12:49:22 +0200

    Immediate posting in home

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -14,8 +14,8 @@ import '***REMOVED***/models/feed/repost_item.dart';
 import 'story_section.dart';
 import 'package:timeago/timeago.dart' as timeago;
 import 'package:flutter/rendering.dart';
-import 'app_bar.dart'; // Import the app_bar.dart
-import 'bottom_navigation_bar.dart'; // Import the bottom_navigation_bar.dart
+import 'app_bar.dart'; 
+import 'bottom_navigation_bar.dart'; 
 import 'shimmer_post_card.dart';
 import 'shimmer_repost_card.dart';
 
@@ -35,7 +35,7 @@ class HomePageState extends State<HomePage> with SingleTickerProviderStateMixin 
   final ScrollController _scrollController = ScrollController();
   int? _userId;
 
-  bool _isBarVisible = true; // Bar visibility flag
+  bool _isBarVisible = true;
   late AnimationController _animationController;
   late Animation<double> _animation;
 
@@ -45,7 +45,6 @@ class HomePageState extends State<HomePage> with SingleTickerProviderStateMixin 
     _initializeApp();
     _scrollController.addListener(_scrollListener);
 
-    // Initialize animation controller
     _animationController = AnimationController(
       vsync: this,
       duration: const Duration(milliseconds: 200),
@@ -56,37 +55,7 @@ class HomePageState extends State<HomePage> with SingleTickerProviderStateMixin 
       curve: Curves.easeInOut,
     );
 
-    // Initially, make sure both bars are visible
-    _animationController.value = 1.0; // Fully expanded (visible)
-  }
-
-  void _scrollListener() {
-    // Hide the AppBar and BottomNavigationBar when scrolling down
-    if (_scrollController.position.userScrollDirection == ScrollDirection.reverse) {
-      if (_isBarVisible) {
-        _animationController.reverse(); // Hide bars
-        setState(() {
-          _isBarVisible = false;
-        });
-      }
-    }
-    // Show the AppBar and BottomNavigationBar when scrolling up
-    else if (_scrollController.position.userScrollDirection == ScrollDirection.forward) {
-      if (!_isBarVisible) {
-        _animationController.forward(); // Show bars
-        setState(() {
-          _isBarVisible = true;
-        });
-      }
-    }
-
-    // Pagination logic: Load more data when reaching near the bottom of the list
-    if (_scrollController.position.pixels >=
-        _scrollController.position.maxScrollExtent - 200 &&
-        !_isFetchingData &&
-        _hasMoreData) {
-      _fetchMoreFeed();
-    }
+    _animationController.value = 1.0;
   }
 
   @override
@@ -96,6 +65,32 @@ class HomePageState extends State<HomePage> with SingleTickerProviderStateMixin 
     _animationController.dispose();
     super.dispose();
   }
+
+  void _scrollListener() {
+    if (_scrollController.position.userScrollDirection == ScrollDirection.reverse) {
+      if (_isBarVisible) {
+        _animationController.reverse();
+        setState(() {
+          _isBarVisible = false;
+        });
+      }
+    } else if (_scrollController.position.userScrollDirection == ScrollDirection.forward) {
+      if (!_isBarVisible) {
+        _animationController.forward();
+        setState(() {
+          _isBarVisible = true;
+        });
+      }
+    }
+
+    if (_scrollController.position.pixels >=
+        _scrollController.position.maxScrollExtent - 200 &&
+        !_isFetchingData &&
+        _hasMoreData) {
+      _fetchMoreFeed();
+    }
+  }
+
   Future<void> _initializeApp() async {
     try {
       bool isLoggedIn = await LoginService().isLoggedIn();
@@ -112,92 +107,91 @@ class HomePageState extends State<HomePage> with SingleTickerProviderStateMixin 
     }
   }
 
-Future<void> _fetchFeed() async {
-  try {
-    setState(() {
-      _isFetchingData = true;
-      _currentPageNumber = 1; // Reset to first page when refreshing
-      _hasMoreData = true;
-    });
-    if (_userId != null) {
-      List<FeedItem> feedItems = await FeedService().fetchFeed(
-        userId: _userId!,
-        pageNumber: _currentPageNumber,
-        pageSize: _pageSize,
-      );
+  Future<void> _fetchFeed() async {
+    try {
       setState(() {
-        _feedItems = feedItems;
-        _isFetchingData = false;
-        _hasMoreData = feedItems.length == _pageSize;
+        _isFetchingData = true;
+        _currentPageNumber = 1;
+        _hasMoreData = true;
       });
-    }
-  } on SessionExpiredException {
-    setState(() {
-      _isFetchingData = false;
-    });
-    if (context.mounted) {
-      handleSessionExpired(context);
-    }
-  } catch (e) {
-    setState(() {
-      _isFetchingData = false;
-    });
-    print('Failed to load feed: $e');
-  }
-}
-
-Future<void> _fetchMoreFeed() async {
-  if (_isFetchingData || !_hasMoreData) return;
-
-  try {
-    setState(() {
-      _isFetchingData = true;
-      _currentPageNumber++;
-    });
-
-    if (_userId != null) {
-      List<FeedItem> newFeedItems = await FeedService().fetchFeed(
-        userId: _userId!,
-        pageNumber: _currentPageNumber,
-        pageSize: _pageSize,
-      );
-
-      if (newFeedItems.isNotEmpty) {
+      if (_userId != null) {
+        List<FeedItem> feedItems = await FeedService().fetchFeed(
+          userId: _userId!,
+          pageNumber: _currentPageNumber,
+          pageSize: _pageSize,
+        );
         setState(() {
-          _feedItems.addAll(
-            newFeedItems.where((newItem) => !_feedItems.contains(newItem)).toList(),
-          );
+          _feedItems = feedItems;
           _isFetchingData = false;
-          _hasMoreData = newFeedItems.length == _pageSize;
-        });
-      } else {
-        setState(() {
-          _isFetchingData = false;
-          _hasMoreData = false;
+          _hasMoreData = feedItems.length == _pageSize;
         });
       }
+    } on SessionExpiredException {
+      setState(() {
+        _isFetchingData = false;
+      });
+      if (context.mounted) {
+        handleSessionExpired(context);
+      }
+    } catch (e) {
+      setState(() {
+        _isFetchingData = false;
+      });
+      print('Failed to load feed: $e');
     }
-  } on SessionExpiredException {
-    setState(() {
-      _isFetchingData = false;
-      _currentPageNumber--; // Decrement page number if fetching fails due to session expiration
-    });
-    if (context.mounted) {
-      handleSessionExpired(context);
-    }
-  } catch (e) {
-    setState(() {
-      _isFetchingData = false;
-      _currentPageNumber--; // Decrement page number if fetching fails
-    });
-    print('Failed to load more feed: $e');
   }
-}
 
-Future<void> _refreshFeed() async {
-  await _fetchFeed();
-}
+  Future<void> _fetchMoreFeed() async {
+    if (_isFetchingData || !_hasMoreData) return;
 
+    try {
+      setState(() {
+        _isFetchingData = true;
+        _currentPageNumber++;
+      });
+
+      if (_userId != null) {
+        List<FeedItem> newFeedItems = await FeedService().fetchFeed(
+          userId: _userId!,
+          pageNumber: _currentPageNumber,
+          pageSize: _pageSize,
+        );
+
+        if (newFeedItems.isNotEmpty) {
+          setState(() {
+            _feedItems.addAll(
+              newFeedItems.where((newItem) => !_feedItems.contains(newItem)).toList(),
+            );
+            _isFetchingData = false;
+            _hasMoreData = newFeedItems.length == _pageSize;
+          });
+        } else {
+          setState(() {
+            _isFetchingData = false;
+            _hasMoreData = false;
+          });
+        }
+      }
+    } on SessionExpiredException {
+      setState(() {
+        _isFetchingData = false;
+        _currentPageNumber--;
+      });
+      if (context.mounted) {
+        handleSessionExpired(context);
+      }
+    } catch (e) {
+      setState(() {
+        _isFetchingData = false;
+        _currentPageNumber--;
+      });
+      print('Failed to load more feed: $e');
+    }
+  }
+
+  Future<void> _refreshFeed() async {
+    await _fetchFeed();
+  }
 
   Widget buildDivider() {
     return Divider(
@@ -229,13 +223,18 @@ Future<void> _refreshFeed() async {
             ),
             Expanded(
               child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
+                onTap: () async {
+                  // Navigate to CreatePostPage and wait for result
+                  final result = await Navigator.push(
                     context,
                     MaterialPageRoute(
                       builder: (context) => const CreatePostPage(),
                     ),
                   );
+                  // If the result is true, refresh the feed
+                  if (result == true) {
+                    _refreshFeed();
+                  }
                 },
                 child: const Text(
                   'What do you want to share',
@@ -274,77 +273,71 @@ Future<void> _refreshFeed() async {
     } else if (item is RepostItem) {
       return RepostCard(feedItem: item);
     } else {
-      return Container(); // Handle unknown types gracefully
+      return Container();
     }
   }
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    backgroundColor: Colors.grey[100], // Neutral background for other areas
-    appBar: PreferredSize(
-      preferredSize: Size.fromHeight(kToolbarHeight),
-      child: SizeTransition(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(kToolbarHeight),
+        child: SizeTransition(
+          sizeFactor: _animation,
+          axisAlignment: -1.0,
+          child: buildTopAppBar(context),
+        ),
+      ),
+      body: RefreshIndicator(
+        onRefresh: _refreshFeed,
+        color: const Color(0xFFF45F67),
+        child: ListView.builder(
+          controller: _scrollController,
+          itemCount: _isFetchingData ? 10 : _feedItems.length + 6,
+          itemBuilder: (context, index) {
+            if (index == 0) {
+              return StorySection(userId: _userId);
+            } else if (index == 1) {
+              return buildDivider();
+            } else if (index == 2) {
+              return buildPostInputSection();
+            } else if (index == 3) {
+              return buildDivider();
+            } else if (_isFetchingData) {
+              return index % 2 == 0
+                  ? const ShimmerPostCard()
+                  : const ShimmerRepostCard();
+            } else if (index - 4 < _feedItems.length) {
+              return Container(
+                color: Colors.grey[100],
+                child: buildFeedItem(_feedItems[index - 4]),
+              );
+            } else if (index == _feedItems.length + 4) {
+              return _hasMoreData
+                  ? Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Center(
+                        child: CircularProgressIndicator(color: const Color(0xFFF45F67)),
+                      ),
+                    )
+                  : const SizedBox.shrink();
+            } else if (index == _feedItems.length + 5) {
+              return Container(
+                height: 100,
+                color: Colors.grey[300],
+              );
+            } else {
+              return const SizedBox.shrink();
+            }
+          },
+        ),
+      ),
+      bottomNavigationBar: SizeTransition(
         sizeFactor: _animation,
-        axisAlignment: -1.0,
-        child: buildTopAppBar(context),
+        axisAlignment: 1.0,
+        child: buildBottomNavigationBar(context),
       ),
-    ),
-    body: RefreshIndicator(
-      onRefresh: _refreshFeed,
-      color: const Color(0xFFF45F67),
-      child: ListView.builder(
-        controller: _scrollController,
-        itemCount: _isFetchingData ? 10 : _feedItems.length + 6, // Show 10 shimmer cards while loading
-        itemBuilder: (context, index) {
-          if (index == 0) {
-            return StorySection(userId: _userId);
-          } else if (index == 1) {
-            return buildDivider();
-          } else if (index == 2) {
-            return buildPostInputSection();
-          } else if (index == 3) {
-            return buildDivider();
-          } else if (_isFetchingData) {
-            // Show shimmer cards for all posts while fetching
-            return index % 2 == 0 
-              ? const ShimmerPostCard() 
-              : const ShimmerRepostCard();
-          } else if (index - 4 < _feedItems.length) {
-            // Show actual posts once data is loaded
-            return Container(
-              color: Colors.grey[100], // Background color for posts and reposts
-              child: buildFeedItem(_feedItems[index - 4]),
-            );
-          } else if (index == _feedItems.length + 4) {
-            // Show loading indicator at the bottom if more data is being fetched
-            return _hasMoreData
-                ? Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Center(
-                      child: CircularProgressIndicator(color: const Color(0xFFF45F67)),
-                    ),
-                  )
-                : const SizedBox.shrink();
-          } else if (index == _feedItems.length + 5) {
-            // Grey background for the area below posts and reposts
-            return Container(
-              height: 100, // Set appropriate height for the grey area
-              color: Colors.grey[300], // Grey color for the bottom
-            );
-          } else {
-            return const SizedBox.shrink();
-          }
-        },
-      ),
-    ),
-    bottomNavigationBar: SizeTransition(
-      sizeFactor: _animation,
-      axisAlignment: 1.0,
-      child: buildBottomNavigationBar(context),
-    ),
-  );
-}
-
-
+    );
+  }
 }
